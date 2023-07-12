837895405e2d2b24c5e0141911407e250bc49fa6
roAscurra <rominaascurra4887@gmail.com>
2023-07-12 10:21:55 -0300

    Ocultando credenciales de firebase

--- js/firebase.js
+++ js/firebase.js
@@ -6,13 +6,13 @@ import { getDatabase, ref, get, child } from "https://www.gstatic.com/firebasejs
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBgEgY67Vg9508eC3_pj57ZaUiIZpNiQ3U",
-    authDomain: "retiros-bd70f.firebaseapp.com",
-    projectId: "retiros-bd70f",
-    storageBucket: "retiros-bd70f.appspot.com",
-    messagingSenderId: "29033270579",
-    appId: "1:29033270579:web:cc0bcc455e9b7c308e8246",
-    measurementId: "G-ET2ELETHS4"
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxx",
+    projectId: "xxxxxxxxx",
+    storageBucket: "xxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    measurementId: "xxxxxxxxx"
 };
 
 // Initialize Firebase
